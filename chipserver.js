d3ef54d4b6a1b3c7b8cf770e410fe676a28cfc8d
Jake <jake.yang@synchronoss.com>
2017-03-17 11:08:52 +0800

    Add output information when server is up

--- chipserver.js
+++ chipserver.js
@@ -2,10 +2,14 @@
 const fs = require('fs');
 const http = require('http');
 const url = require("url");
+const os = require("os");
+
 const chipStore = {};
 
-const WRITEWORK_INTERVAL = 1 * 30 * 1000;
-const WRITE_SINCE_UPDATE = 1 * 60 * 1000;
+const CHECK_WRITE_INTERVAL = 1 * 30 * 1000;
+const WRITE_DELAY_SINCE_UPDATE = 1 * 60 * 1000;
+const MACHINE_IP = os.networkInterfaces().eth1 ? os.networkInterfaces().eth1[0].address : 'UNKNOWN';
+const SERVICE_PORT = 8080;
 
 const server = http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname;
@@ -15,9 +19,14 @@ const server = http.createServer(function (req, res) {
         invalidResponse(res);
     }
 
-}).listen(8080);
+}).listen(SERVICE_PORT);
 
-setInterval(writeWork, WRITEWORK_INTERVAL)
+console.log('Chips server(', MACHINE_IP, ':'+SERVICE_PORT, ') is up...');
+console.log("CHECK_WRITE_INTERVAL:", CHECK_WRITE_INTERVAL,'ms');
+console.log("WRITE_DELAY_SINCE_UPDATE:", WRITE_DELAY_SINCE_UPDATE, 'ms');
+
+
+setInterval(writeWork, CHECK_WRITE_INTERVAL)
 
 
 function invalidResponse(res) {
@@ -73,13 +82,11 @@ function feedMe(chips) {
         chipStore[newtimestamplink] = chips;
         chips.updatedAt = new Date().getTime();
         console.log("append chips", timestamplink, newtimestamplink)
-
     } else {
         const actions = chipJson.actions;
         const timestamplink = actions[actions.length - 1][0];
         chipStore[timestamplink] = chipJson;
         chipJson.updatedAt = new Date().getTime();
-
         console.log("new chips link", timestamplink)
     }
 
@@ -88,9 +95,9 @@ function feedMe(chips) {
 function writeWork() {
     const now = new Date();
     for (let link in chipStore) {
-        if (now - chipStore[link].updatedAt > WRITE_SINCE_UPDATE) {
+        if (now - chipStore[link].updatedAt > WRITE_DELAY_SINCE_UPDATE) {
             console.log("write", chipStore[link].updatedAt, chipStore[link].actions.length);
-            write('\n'+JSON.stringify(chipStore[link]));
+            write('\n' + JSON.stringify(chipStore[link]));
             delete chipStore[link];
         }
     }
@@ -102,6 +109,7 @@ function appendArray(arr0, arr1) {
 
 function write(str) {
     const file = 'datastore/data.log';
+    //str example value: {"site":"www2",actions:[[1489569331453,"mail_message_item"], [1489569331453,"mail_compose_tbbtn"], [1489569331453,"compose_editor_input"],[1489569331453,"compose_to_input"], [1489569331454,"compose_send_btn"]]}
     fs.appendFileSync(file, str);
     return str.length;
 }
