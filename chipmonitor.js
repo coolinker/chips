8832785b0635209c103704b166e889b57ec0c0e7
Jake <jake.yang@synchronoss.com>
2017-03-24 22:24:19 +0800

    optimized the element parser functions by using css selector and regex

--- chipmonitor.js
+++ chipmonitor.js
@@ -1,30 +1,42 @@
 /**
 <script type="text/javascript" src="https://raw.githack.com/coolinker/chips/master/chipmonitor.js"></script>
 <script type="text/javascript">
-    ChipMonitor({version:'2.2.101', sendDelay:10000,chipService:'http://10.37.2.237:8080/chips', debug:true});
+    ChipMonitor({version:'2.2.101', sendDelay:30000,chipService:'http://10.37.10.211:8080/chips', debug:true});
 </script> 
  */
+
 function ChipMonitor(options) {
+    if (options.stop) {
+        stopMonitor();
+        return;
+    }
+
     var SERIAL = parseInt(Math.random() * 100000000000000000, 10);
     var ORIGIN = options.origin ? options.origin : window.location.hostname;
+    var ext = window.location.search.match(/(?:originext=)([A-Za-z0-9_-]+)/g);
+    if (ext) {
+        ORIGIN += '-'+ext[0].split('=')[1];
+    }
+
     var BATCH = options.version;
     var SENDDELAY = options.sendDelay ? options.sendDelay : 1 * 60 * 1000;
     var CHIPSERVICE = options.chipService;
+
     var debugMode = options.debug;
-    var objectParser = options.objectParser ? options.objectParser : defaultObjectParser;
+    var identifierParser = options.findElementIdentifier ? options.findElementIdentifier : findElementIdentifier;
     console.log("Chip monitor is up:", ORIGIN, BATCH, SENDDELAY, CHIPSERVICE)
     var actions = [];
     var timeoutObj;
 
     document.addEventListener('click', clickHandler, true);
-
     function clickHandler(e) {
-        var info = objectParser(e);
-        if (!info.key) {
+        var key = identifierParser(e.target);
+        if (!key) {
             if (debugMode) console.log('no key target', e.target);
             return;
         }
-        actions.push([new Date().getTime(), info.key, info]);
+        if (debugMode) console.log('push action:', key);
+        actions.push([new Date().getTime(), key.trim()]);
         resetSendDelay();
     };
 
@@ -43,11 +55,10 @@ function ChipMonitor(options) {
         xhr.onreadystatechange = function () {
             if (xhr.readyState !== 4) return;
             if (xhr.status == 200) {
-                //console.log('Chipservice done', xhr.responseText)
                 resetActions(timestamp);
             } else {
                 console.log("Chip server seems not availible. Disable ChipMonitor funciton.", xhr.status, new Date());
-                document.removeEventListener('click', clickHandler)
+                stopMonitor();
             }
         }
 
@@ -71,135 +82,97 @@ function ChipMonitor(options) {
         }
     };
 
-    function defaultObjectParser(e) {
-        var classname, ancestor, ancestorText, text, name;
-        classname = classnameParser(e.target);
-        //if (debugMode) console.log('defaultObjectParser', e.target);
-        if (classname.length === 0) {
-            if (ancestor = e.target.closest('.rui-cal-picker')) {
-                classname = '.rui-cal-picker';
-            } else if (ancestor = e.target.closest('.rui-calist-datalist')) {
-                classname = '.rui-calist-datalist';
-            } else if (ancestor = e.target.closest('.cal-list-evt')) {
-                classname = '.cal-list-evt';
-            } else if (ancestor = e.target.closest('.rui-cal-evt-tbl-title')) {
-                classname = '.rui-cal-evt-tbl-title';
-            } else if (ancestor = e.target.closest('.rui-cal-ev')) {
-                classname = '.rui-cal-ev';
-            } else if (ancestor = e.target.closest('.rui-settings-list')) {
-                classname = '.rui-settings-list';
-            } else if (ancestor = e.target.closest('.rui-pab-entry-alphabet-strip-item')) {
-                classname = '.rui-pab-entry-alphabet-strip-item';
-            } else if (e.target.className.contains('x-form-date-trigger')) {
-                classname = 'x-form-data-trigger';
-                ancestor = e.target;
-            } else if (e.target.className.contains('x-trigger-index-0')) {
-                var parentNode = e.target.parentNode;
-                var previousSibling = parentNode.previousSibling;
-                classname = classnameParser(previousSibling);
-            } else if (e.target.className.contains('x-grid-row-checker')) {
-                classname = 'x-grid-row-checker';
-            } else if (ancestor = e.target.closest('.x-datepicker-cell')) {
-                classname = 'x-datepicker-cell';
-            } else if (ancestor = e.target.closest('.x-message-box')) {
-                classname = 'x-message-box';
-                if (!text) {
-                    var previousSibling = e.target.previousSibling;
-                    text = previousSibling.innerText || previousSibling.textContent;
+    function getElementIdentifier(ele) {
+        if (!ele) return;
+        var regex = /(^(rui|ow(?!-autoDir)|cal|automation)| (rui|ow(?!-autoDir)|cal|automation))[^ ]*/g;
+        var cls = ele.className;
+        var selected;
+        var identifier;
+        if (selected = cls.match(regex)) {
+            identifier = selected.join();
+        } else if (cls.match(/x-form-date-trigger/g)) {
+            identifier = 'x-form-date-trigger';
+        } else if (cls.match(/x-boundlist/g)) {
+            identifier = cls + '-' + getElementIndex(ele)
+        }
+
+        return identifier;
+    }
+
+    function findElementIdentifier(ele) {
+        var identifier = getElementIdentifier(ele);
+        if (identifier) return identifier;
+        var regex = /(^(rui|ow(?!-autoDir)|cal|automation)| (rui|ow(?!-autoDir)|cal|automation))[^ ]*/g;
+        var selector = '[class^=rui-],[class*=" rui-"],:not([class=ow-autoDir])[class^=ow-],[class*=" ow-"],[class^=cal-],[class*=" cal-"],[class^=automation-],[class*=" automation-"]';
+
+        var ancestor = ele.closest('.x-grid-row, .x-menu-item, .x-form-checkbox, .x-message-box, .x-btn, .rui-cal-picker, .rui-calist-datalist, .cal-list-evt, .rui-cal-evt-tbl-title, .rui-cal-ev, .rui-settings-list, .rui-pab-entry-alphabet-strip-item, .x-datepicker-cell')
+        if (ancestor) {
+            var acls = ancestor.className;
+            if (acls.indexOf('x-message-box') > -1) {
+                var previousSibling = ele.previousSibling;
+                var text = previousSibling.textContent;
+                return 'x-message-box-' + text;
+            } else if (acls.indexOf('x-grid-row') > -1) {
+                var rowidt, folderele;
+                if (folderele = ancestor.querySelector('.ow-icon-mail-folderConlumn-inner')) rowidt = folderele.className;
+                else if (folderele = ancestor.querySelector('.ow-addressbook-name')) rowidt = folderele.className;
+                if (rowidt) return rowidt;
+
+                if (ancestor.querySelector('[class*=mail]')) rowidt = 'x-grid-row-mail';
+                else if (ancestor.querySelector('[class*=contact]')) rowidt = 'x-grid-row-contact';
+
+                if (ele.querySelector('[class*=checker]') || ele.className.indexOf('-checker') > -1) {
+                    rowidt += '-checker';
                 }
-            } else if (e.target.tagName === 'TEXTAREA' || e.target.tagName === 'INPUT') {
-                ancestor = e.target;
-                classname = classnameParser(ancestor);
-                name = ancestor.name;
-            } else if (ancestor = e.target.closest('.x-grid-row')) {
-                classname = 'x-grid-row';
-                if (ancestor.getElementsByClassName('mail-list-icon-column').length > 0 || ancestor.getElementsByClassName('rui-mail-headerlist-row').length > 0) classname = 'mail-x-grid-row';
-            } else if ((ancestor = e.target.closest('.x-btn'))
-                || (ancestor = e.target.closest('.x-menu-item'))
-                || (ancestor = e.target.closest('.x-form-checkbox'))) {
-                classname = classnameParser(ancestor);
-            } else {
-                if (debugMode) console.log('target unknown', e.target);
-            }
-        }
+                return rowidt;
+            } else if (acls.indexOf("x-menu-item") > -1) {
+                selected = acls.match(regex);
+                if (selected) return selected.join() + '-' + getElementIndex(ancestor);
+                return 'x-menu-item-' + getElementIndex(ancestor);
+            } else if (acls.match(/x-btn/g)) {
+                var btnid = getElementIdentifier(ancestor);
+                if (btnid) return btnid + '-' + ancestor.textContent;
 
-        if (!text) {
-            text = e.target.textContent || e.target.innerText || e.target.outerText;
-            if (ancestor) {
-                ancestorText = ancestor.innerText;
-                text = text || ancestorText;
-            }
-        }
-
-        var key = name ? name : classname.toString();
-        if (key.indexOf('x-boundlist') > -1) {
-            key = key + "-" + getElementIndex(e.target);
-        } else if (key.indexOf('x-menu-item') > -1) {
-            var item = e.target.closest('.x-menu-item');
-            key = key + "-" + getElementIndex(item);
-        } else if (key.indexOf('x-message-box') > -1) {
-            key = key + '-' + text;
-        }
-
-        var obj = {};
-        if (key) obj.key = key;
-        if (classname.length > 0) obj.classname = classname.toString();
-        if (text) obj.text = text;
-        if (name) obj.name = name;
-
-        if (debugMode) console.log(obj);
-
-        return obj;
-
-    };
-
-    function classnameParser(ancestor) {
-        var ownClassName = [];
-        var ancestorClassName = ancestor.classList;
-        for (var k = 0; k < ancestorClassName.length; k++) {
-            if ((ancestorClassName[k].startsWith('rui') || ancestorClassName[k].startsWith('ow') || ancestorClassName[k].startsWith('cal')
-                || ancestorClassName[k].startsWith('automation') || ancestorClassName[k].startsWith('x-menu-item') || ancestorClassName[k].startsWith('x-boundlist')) && ancestorClassName[k] !== 'ow-autoDir') {
-                ownClassName.push(ancestorClassName[k]);
-            }
-        }
-        if (ownClassName.length >= 1) {
-            return ownClassName;
-        }
-
-        var children = ancestor.childNodes;
-        if (children.length === 0) {
-            return ownClassName;
-        }
-        for (var i = 0; i < children.length; i++) {
-            if (children[i].nodeType === 3) {
-                return ownClassName;
-            }
-            var childClassList = children[i].classList;
-            for (var j = 0; j < childClassList.length; j++) {
-                if ((childClassList[j].startsWith('rui') || childClassList[j].startsWith('ow') || childClassList[j].startsWith('cal')
-                    || childClassList[j].startsWith('automation')) && childClassList[j] !== 'ow-autoDir') {
-                    ownClassName.push(childClassList[j]);
+                var xbox = ancestor.closest('.x-message-box');
+                if (xbox) {
+                    return 'x-message-box-' + ancestor.textContent;
                 }
-            }
 
-            if (ownClassName.length >= 1) {
-                return ownClassName;
-            }
-            else {
-                ownClassName = classnameParser(children[i]);
+                var xwin = ancestor.closest('.x-window');
+                if (xwin) {
+                    return 'x-window-' + getElementIdentifier(xwin) + ancestor.textContent;
+                }
+
+                btnid = getElementIdentifier(ancestor.querySelector(selector));
+                if (btnid) return btnid + ancestor.textContent;
+            } else if (acls.match(/x-form-checkbox/g)) {
+                return 'x-form-checkbox';
             }
         }
 
-        if (!ownClassName) ownClassName.ancestor.classList;
 
-        return ownClassName;
-    };
+        if (ele.tagName === 'TEXTAREA' || ele.tagName === 'INPUT') {
+            return ele.name;
+        }
 
+        var closest = ele.closest(selector);
+        if (closest) {
+            identifier = getElementIdentifier(closest);
+            if (identifier) return identifier;
+        }
+
+        return 'uncaught-identifier-' + ele.className;
+    }
 
     function getElementIndex(node) {
         var i = 1;
         while (node = (node.previousElementSibling || node.previousSibling)) { ++i }
         return i;
+    }
+
+    function stopMonitor() {
+        document.removeEventListener('click', clickHandler)
+        if (timeoutObj) clearTimeout(timeoutObj);
     }
 }
 
